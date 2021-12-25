2a11eeb1a28e6e9c209b13ba1a834d0577541317
niteshkumartiwari <niteshsoftwaredeveloper@gmail.com>
2021-12-25 13:19:39 +0530

    adding oauth2 backend

--- backend/app/app/src/main/java/com/feedloop/app/resource/UserController.java
+++ backend/app/app/src/main/java/com/feedloop/app/resource/UserController.java
@@ -5,9 +5,11 @@ import com.feedloop.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
--- backend/app/app/src/main/java/com/feedloop/app/security/ApplicationSecurity.java
+++ backend/app/app/src/main/java/com/feedloop/app/security/ApplicationSecurity.java
@@ -1,22 +1,21 @@
 package com.feedloop.app.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
-    @Profile("test")
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**");
-    }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .antMatcher("/**").authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+
     }
 }
