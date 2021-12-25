95ac605af34f0d25371b0da00f2028a11e7c315a
niteshkumartiwari <niteshsoftwaredeveloper@gmail.com>
2021-12-25 19:49:49 +0530

    removing extra buttons

--- frontend/feedbackapp/src/components/HeroSection.js
+++ frontend/feedbackapp/src/components/HeroSection.js
@@ -9,22 +9,6 @@ function HeroSection() {
       <video src="/videos/cover.mp4" autoPlay loop muted />
       <h1>Grow Personally</h1>
       <p>Lets help each other grow!</p>
-      <div className="hero-btns">
-        <Button
-          className="btns"
-          buttonSize="btn--large"
-          buttonStyle="btn--outline"
-        >
-          GET STARTED
-        </Button>
-        <Button
-          className="btns"
-          buttonSize="btn--large"
-          buttonStyle="btn--primary"
-        >
-          WATCH TRAILER <i className="far fa-play-circle"></i>
-        </Button>
-      </div>
     </div>
   );
 }
